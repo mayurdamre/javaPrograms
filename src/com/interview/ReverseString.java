200a4663c6a6f91568f6e69617e66f077f4c22b7
mayurdamre <mayurdamre111@gmail.com>
2024-09-24 12:40:21 +0530

    first of today for reverse string program

---
+++ src/com/interview/ReverseString.java
@@ -0,0 +1,47 @@
+package com.interview;
+
+import java.util.Scanner;
+
+public class ReverseString {
+	
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter a String "); //mayur
+		String name = sc.nextLine();
+		String rev = "";
+		
+		sc.close();
+		
+		// Logic 1 : algorithm
+		
+//		for(int i=name.length()-1; i>=0; i--) {
+//			rev= rev + name.charAt(i);
+//		}
+//		System.out.println("Reverse String is "+ rev);
+		
+		// Logic 2 : using character array
+		
+		char a[]=name.toCharArray();
+		for(int j=name.length()-1; j>=0; j--) {
+			rev = rev + a[j];
+		}
+		System.out.println("Reverse String is "+rev);
+		
+	
+		// Logic 3 : By using StringBuffer class
+//		StringBuffer sb = new StringBuffer(name);
+//		StringBuffer rev1 = sb.reverse();
+//		
+//		System.out.println("Reverse String is "+rev1);
+		
+		// Logic 4 : By using String Builder Class
+		
+//		StringBuilder sbl = new StringBuilder();
+//		sbl.append(name);
+//		StringBuilder rev2 = sbl.reverse();
+//		System.out.println("Reverse String By StringBuilder class :-  "+rev2);
+		
+	}
+
+}
